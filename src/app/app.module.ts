3d6ead2b1622090f55996512200dbd9bde553ae2
Julie <juli.bilovus@gmail.com>
2017-09-07 18:38:21 +0300

    errors fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
 import { HomepageComponent } from './homepage/homepage.component';
+
+import { RedactorPageComponent } from './redactor-page/redactor-page.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    RedactorPageComponent,
+    routingComponents
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
