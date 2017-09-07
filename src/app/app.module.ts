06b80a45460797cf94dea7b00bc5d660232dfb65
khrystynad <dalivskakhrystyna@gmail.com>
2017-09-07 14:06:55 +0300

    add order-page component and child make-order component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MaterialModule } from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/order-page/make-order-page/make-order-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-make-order-page',
+  templateUrl: './make-order-page.component.html',
+  styleUrls: ['./make-order-page.component.scss']
+})
+export class MakeOrderPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
