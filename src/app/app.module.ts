78f2bf9a5678d3990973903e7b49a1bc2a60223f
Steve <steve.neeson21@gmail.com>
2017-09-09 17:40:59 +0300

    Added profile-page side bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,12 @@ import { RouterModule } from '@angular/router';
 import { AppRoutingModule, routingComponents } from './app.routing.module';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MdToolbarModule } from '@angular/material';
 import { MdIconModule } from '@angular/material';
+import { MdGridListModule } from '@angular/material';
+import { MdSidenavModule } from '@angular/material';
+import { MdButtonModule } from '@angular/material';
 
 import { ProfilePageModule } from './profile-page/profile-page.module';
 
@@ -26,8 +30,12 @@ import { FooterComponent } from './components/footer/footer.component'
     BrowserModule,
     AppRoutingModule,
     ProfilePageModule,
+    BrowserAnimationsModule,
     MdToolbarModule,
-    MdIconModule
+    MdIconModule,
+    MdGridListModule,
+    MdSidenavModule,
+    MdButtonModule
   ],
   providers: [],
   bootstrap: [
