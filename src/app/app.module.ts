f3f722658a9775a8c41ce356eab4c0e7575a8504
Steve <steve.neeson21@gmail.com>
2017-09-09 13:49:31 +0300

    Added simple header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,29 @@ import { RouterModule } from '@angular/router';
 import { AppRoutingModule, routingComponents } from './app.routing.module';
 import { NgModule } from '@angular/core';
 
+import { MdToolbarModule } from '@angular/material';
+import { MdIconModule } from '@angular/material';
+
 import { ProfilePageModule } from './profile-page/profile-page.module';
 
 
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './components/header/header.component'
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    routingComponents
+    routingComponents,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ProfilePageModule
+    ProfilePageModule,
+    MdToolbarModule,
+    MdIconModule
   ],
   providers: [],
   bootstrap: [
