8f00a6e4ab7a45f9ff47688e6ac14d3ab0fc4bc1
Steve <steve.neeson21@gmail.com>
2017-09-12 19:39:17 +0300

    commit before pulling

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,29 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import 'rxjs/Rx';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 
 @Injectable()
 export class UserService {
 
-    constructor(private _http: Http){}
-/*     getUser(){
-        return {
-            id: 1,
-            firstName: 'Tom',
-            lastName: 'Smith',
-            email: 'tom.smith@gmail.com',
-            avatar: '/assets/images/avatars/myAvatar.png',
-            products: ['T-shirt with label cat', 'Green cup with name']
-        }    
-    } */
-    getUser(){
-        return this._http.get('https://kolibri-7dd6a.firebaseio.com/user.json')
-            .map(response => response.json());
+    constructor(){
     }
 
-    setUser(firstName: string, lastName:string){
-        const body = JSON.stringify({firstName: firstName, lastName: lastName});
-        return this._http.put('https://kolibri-7dd6a.firebaseio.com/user.json', body)
-            .map(response => response.json());
-    }
 }
