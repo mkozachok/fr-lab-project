bdff342b04db231bf97c925e3cdb4dd5040adad1
Steve <steve.neeson21@gmail.com>
2017-09-17 00:23:58 +0300

    added loading while whole app loads

--- src/app/components/admin-page/admin-page.component.ts
+++ src/app/components/admin-page/admin-page.component.ts
@@ -10,7 +10,9 @@ import { Component, OnInit } from '@angular/core';
 export class AdminPageComponent implements OnInit {
   
 
-  constructor() { }
+  constructor() {
+    
+  }
 
   ngOnInit(): void {
 
