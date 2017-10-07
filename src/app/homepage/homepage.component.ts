f29e8d1045e7b4bf9b2f572d5af76b503e4f9337
Julie <juli.bilovus@gmail.com>
2017-10-07 16:56:01 +0300

    change some styles at the homepage and redactor page, relocate color choose button at the redactor page

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -82,7 +82,7 @@ export class HomepageComponent implements OnInit {
     });
     this.productListService.getProducts().subscribe(items => {
       this.showSpinner = false;
-      this.prods = items;
+      this.prods = items.reverse();
       this.arrOfProds = this.prods;
     });
     //this.productListService.getProducts2(this.startAt, this.endAt).subscribe(items => this.prods = items);
@@ -97,7 +97,7 @@ export class HomepageComponent implements OnInit {
 
   sorting(propValue) {
     this.productListService.selectProducts(propValue).subscribe(res => {
-      this.prods = res;
+      this.prods = res.reverse();
     });
   }
 
